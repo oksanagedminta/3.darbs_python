0aa215f567dcf08d8c9db31e8738344cfbd49219
Oksana <oksana.tkaca@gmail.com>
2022-04-15 13:55:52 +0300

    tuzd_Risinājums

--- 7uzdevums.py
+++ 7uzdevums.py
@@ -3,3 +3,11 @@ Uzrakstiet programmu Python, lai aprēķinātu
 trīs ievadīto skaitļu summu.
 Ja ievadītās vērtības ir vienādas, atgrieziet summu, trīskāršojot to.
 """
+# ?? atgrieziet summu, trīskaršojot to ??
+a=int(input(" Ievadiet 1. skaitli: "))
+b=int(input(" Ievadiet 2. skaitli: "))
+c=int(input(" Ievadiet 3. skaitli: "))
+sum=a+b+c
+if a==b==c:
+    print(sum*3)
+else: print(sum)
