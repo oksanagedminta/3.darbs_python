6ee328cfa520edceef8566cb728a3788e16f0569
Oksana <oksana.tkaca@gmail.com>
2022-04-15 12:14:20 +0300

    3uzd_Atrisinats

--- 3uzdevums.py
+++ 3uzdevums.py
@@ -4,3 +4,13 @@ pirmās un pēdējās krāsas no šī saraksta.
 
 krasa_saraksts = ["sarkans", "zaļš", "balts", "melns"]
 """
+# Nr.1
+krasa_saraksts=["sarkans", "zaļš", "balts", "melns"]
+print(krasa_saraksts[0], krasa_saraksts[3])
+
+# Nr.2
+print(krasa_saraksts[0])
+print(krasa_saraksts[3])
+
+#Nr.3
+print(krasa_saraksts[-4], krasa_saraksts[-1])
