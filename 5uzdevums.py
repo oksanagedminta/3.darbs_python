dc7b81cbb342b005b99e718a0d6549d7baa07411
Oksana <oksana.tkaca@gmail.com>
2022-04-15 13:52:47 +0300

    5uzd_Atrisināts

--- 5uzdevums.py
+++ 5uzdevums.py
@@ -4,3 +4,9 @@ starpību starp ievadīto skaitli un 17.
 Ja skaitlis ir lielāks par 17, iegūstiet absolūtās starpības 
 dubultu.
 """
+# !!Nesaprotu uzdevumu!!!
+
+x=int(input("Ievadiet skaitli: "))
+if x >17:
+    print((x-17)*2)
+else: print(17-x)
