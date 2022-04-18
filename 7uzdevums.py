d0e437f74459423f182b2125caa3eb6888475772
Oksana <oksana.tkaca@gmail.com>
2022-04-19 00:08:39 +0300

    7uzd_Risinu_caur_def

--- 7uzdevums.py
+++ 7uzdevums.py
@@ -3,7 +3,15 @@ Uzrakstiet programmu Python, lai aprēķinātu
 trīs ievadīto skaitļu summu.
 Ja ievadītās vērtības ir vienādas, atgrieziet summu, trīskāršojot to.
 """
-# ?? atgrieziet summu, trīskaršojot to ??
+# Nr.1 ar FUNKCIJU def
+def plus(x,y,z):
+    plus=x+y+z
+    if x==y==z:
+        return plus*3
+meginu=plus(2,2,2)
+print(meginu)
+
+# Nr.2
 a=int(input(" Ievadiet 1. skaitli: "))
 b=int(input(" Ievadiet 2. skaitli: "))
 c=int(input(" Ievadiet 3. skaitli: "))
